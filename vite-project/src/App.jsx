5da392b1f862c6196e6e6ff829ee905c4c679657
Janak Bahadur Bohara <janakcustomx@gmail.com>
2024-07-10 08:02:57 +0545

    create react-app and vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+function App() {
+
+  return (
+    <>
+    <Header />
+   <h1>Vite React App</h1>
+   <p>This is a react app</p>
+
+    </>
+);
+}
+
+export default App
